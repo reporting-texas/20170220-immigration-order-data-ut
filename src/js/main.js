68763f0dff13bda962221d1d5906bee899d49d26
Cody Winchester <cjwinchester@gmail.com>
2017-02-19 21:32:05 -0600

    updated styles

--- src/js/main.js
+++ src/js/main.js
@@ -6,20 +6,38 @@
   var sections = document.querySelectorAll('div.data-section');
   var navigate = document.querySelectorAll('span.navigate');
 
-  function showHide(target_div) {
+  var div_showing = sections[0];
+
+  function showHide(navigate_button) {
+    var target = navigate_button.getAttribute('data-div-id');
     for (var i=0; i<sections.length; i++) {
       sections[i].style.display = 'none';
     };
+    var target_div = document.getElementById(target);
     target_div.style.display = 'block';
     pymChild.sendHeight();
+    div_showing = target_div;
   }
 
   for (var e=0; e<navigate.length; e++) {
     navigate[e].addEventListener('click', function() {
-      var target = this.getAttribute('data-div-id');
-      var target_div = document.getElementById(target);
-      showHide(target_div);
+      showHide(this);
     });
   };
+
+  document.addEventListener('keyup', function(e) {
+    if (+e.keyCode === 37) {
+      var back = div_showing.querySelector('span.navigate.navigate-back');
+      if (back !== null) {
+        showHide(back);
+      }
+    }
+    if (+e.keyCode === 39) {
+      var forward = div_showing.querySelector('span.navigate.navigate-forward');
+      if (forward !== null) {
+        showHide(forward);  
+      }
+    }
+  });
 
 })();
